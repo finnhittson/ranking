7012b6c7032cedc1668bec67eae61ea41db2e05d
finnhittson <hittson.finn@gmail.com>
2022-11-19 17:03:44 -0500

    util.py: add get_reviewers() function selects viewers based on how many reviews they have left

---
+++ util.py
@@ -0,0 +1,22 @@
+import csv
+import numpy as np
+
+def get_reviewers(ratings_data_path, min_review_count):
+	total_reviews = []
+	with open(ratings_data_path, newline = '') as csvfile:
+		spamreader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
+		userId = 1
+		review_count = 0
+		ratings = []
+		for row in spamreader:
+			if review_count == min_review_count:
+				total_reviews.append(ratings)
+				ratings = []
+				review_count = 0
+				userId += 1
+			if userId == int(row[0]):
+				review_count += 1
+				ratings.append(float(row[2]))
+		if review_count == min_review_count:
+			total_reviews.append(ratings)
+	return numpy.array(total_reviews)
