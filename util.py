5e84a79305328fc77f4a0bbe2b2d130969956123
finnhittson <hittson.finn@gmail.com>
2022-11-20 19:48:07 -0500

    util.py: restructured csv parser

--- util.py
+++ util.py
@@ -1,24 +1,41 @@
 import csv
 import numpy as np
+import math
 
 def get_reviewers(ratings_data_path, min_review_count):
 	total_reviews = []
 	with open(ratings_data_path, newline = '') as csvfile:
 		spamreader = csv.reader(csvfile, delimiter = ',', quotechar = '|')
-		userId = 1
+		userId = -math.inf
 		review_count = 0
 		ratings = []
 		for idx, row in enumerate(spamreader):
-			if idx == 0:
+			
+			if idx == 0 and row[0] == 'userId':
 				continue
-			if review_count == min_review_count:
-				total_reviews.append(ratings)
+
+			if userId < int(row[0]):
+				userId = int(row[0])
 				ratings = []
 				review_count = 0
-				userId += 1
+
 			if userId == int(row[0]):
 				review_count += 1
 				ratings.append(float(row[2]))
-		if review_count == min_review_count:
-			total_reviews.append(ratings)
-	return np.array(total_reviews).T
+
+			if review_count == min_review_count:# and idx < 500000:
+				ratings = [userId] + ratings
+				total_reviews.append(ratings)
+				userId += 1
+				ratings = []
+				review_count = 0
+
+	return total_reviews
+
+def write_to_file(data, file_path):
+	with open(file_path, 'w', newline='') as f:
+		write = csv.writer(f)
+		for line in data:
+			userId = line.pop(0)
+			for rating in line:
+				write.writerow([userId, rating])
