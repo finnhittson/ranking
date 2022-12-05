48cd823a1dc3909c31281089ee3032215b51c5e6
finnhittson <hittson.finn@gmail.com>
2022-12-04 21:12:06 -0500

    pranking.py: slight restructuring

--- src/pranking.py
+++ src/pranking.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 def pranking(reviews, cycles):
 	reviews = np.array(reviews)
 	w = np.zeros((1, len(reviews) - 1)) # weights and bias initialized to zero
-	b = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	b = np.zeros((1, 10))
 
 	for _ in range(cycles):
 
@@ -20,36 +20,36 @@ def pranking(reviews, cycles):
 		random_viewer = int(random.uniform(0,len(reviews)))
 		np.random.shuffle(reviews)
 		
-		target_rank = copy.copy(reviews[random_viewer, :]) # true labels
+		target_rank = copy.copy(reviews[random_viewer, :]) * 2# true labels
 		reviews = np.delete(reviews,random_viewer,0) # rest of the data
-		
 		for idx, x in enumerate(reviews.T):
+
 			# x is the set of reviews from different people
 			w_dot_x = w.dot(np.array([x]).T)
-			
-			predicted_rank = predict_rank(w_dot_x, b)
+			predicted_rank = predict_rank(w_dot_x[0][0], b)
+
 			# if predicted rank is wrong then update w and b
-			if target_rank[idx] != predicted_rank / 2:
+			if target_rank[idx] != predicted_rank:
 				
 				correction_vect = []
-				for r in range(len(b)):
+				for r in range(len(b[0])):
 					if target_rank[idx] <= r:
 						correction_vect.append(-1)
 					else:
 						correction_vect.append(1)
 				
 				tau = []
-				for r in range(len(b)):
-					if (w_dot_x[0][0] - b[r]) * correction_vect[r] <= 0: # incorrect
+				for r in range(len(b[0])):
+					if (w_dot_x[0][0] - b[0][r]) * correction_vect[r] <= 0: # incorrect
 						tau.append(correction_vect[r])
 					else: # correct
 						tau.append(0)
 				
 				# update w and b
-				w = w + sum(tau) * x / 100
-				for r in range(len(b)):
-					b[r] = b[r] - tau[r]
-		
+				w = w + sum(tau) * x
+				for r in range(len(b[0])):
+					b[0][r] = b[0][r] - tau[r]
+			
 		# re-append target ranks to total data set
 		reviews = np.concatenate((reviews, np.array([target_rank])), axis=0)
 	
@@ -60,8 +60,8 @@ def pranking(reviews, cycles):
 def predict_rank(w_dot_x, b):
 	yt_hat = 10 # max rank
 	r = 0
-	while r < len(b):
-		if w_dot_x - b[r] < 0 and r < yt_hat:
+	while r < len(b[0]):
+		if w_dot_x - b[0][r] < 0 and r < yt_hat:
 			yt_hat = r
 		r += 1
 	return yt_hat
@@ -78,15 +78,17 @@ def run_pranking(data_path, review_count):
 	w, b = pranking(train_reviews,1)
 	print(w)
 	print(b)
- 	
+
 	# evaluate
+	print("eval")
 	correct_count = 0
 	random_viewer = int(random.uniform(0,len(train_reviews)))
 	true_ranks = copy.copy(train_reviews[random_viewer, :])
 	train_reviews = np.delete(train_reviews, random_viewer, 0)
 	predicted_rank = []
 	
-	for idx, review in enumerate(train_reviews.T):			
+	for idx, review in enumerate(train_reviews.T):
+		
 		predicted_rank.append(predict_rank(w.dot(review), b) / 2)
 
 		if predicted_rank[-1] == true_ranks[idx]:
